1cfebe7dc64874974276ebfab403965ab239da7d
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-01-13 08:28:51 -0300

    Update Register.jsx

--- client/src/Pages/Register/Register.jsx
+++ client/src/Pages/Register/Register.jsx
@@ -1,9 +1,19 @@
-import React from "react"
+import React from 'react'
+import {Link} from "react-router-dom"
+import "./Register.css"
 
 const Register = () => {
-    return(
-        <div>
-            Register
+    return (
+        <div className='auth'>
+            <h1>Registra-se</h1>
+            <form>
+                <input required type="text" placeholder='UserName'/>
+                <input required type="email" placeholder='Email'/>
+                <input required type="password" placeholder='Password'/>
+                <button>Register</button>
+                <p>Error de Registro!!</p>
+                <span><Link to="/login">Login</Link></span>
+            </form>
         </div>
     );
 };
