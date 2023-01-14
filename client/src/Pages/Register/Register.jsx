d5035d84f7d2af1efb566e3fdbfc10cd62360b52
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-01-14 08:26:08 -0300

    Update Register.jsx

--- client/src/Pages/Register/Register.jsx
+++ client/src/Pages/Register/Register.jsx
@@ -1,17 +1,45 @@
-import React from 'react'
-import {Link} from "react-router-dom"
+import React, { useState } from 'react'
+import {Link, useNavigate} from "react-router-dom"
+import axios from "axios"
 import "./Register.css"
 
 const Register = () => {
+    const [inputs, setInputs] = useState({
+        username: "",
+        email:"",
+        password: "",
+    });
+
+    const [err, setErr] = useState(null);
+
+    const navigate = useNavigate();
+
+    //atualizando os input state de acordo com oq o usuario escreve
+    const handleChange = (e) => {
+        setInputs(prev => ({...prev, [e.target.name]: e.target.value}))
+    }
+
+    //caso esteja tudo ok usamos o useNavigate para passar a gente para a tela de login
+    //caso tenha algum error de já existir o email, passamos para o err state e caso ele exista a gente renderiza na tela
+    const handleSumit =  async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post("/auth/register", inputs);
+            navigate("/login");
+        } catch (error) {
+            setErr(error.response.data);
+        }
+    }
+
     return (
         <div className='auth'>
             <h1>Registra-se</h1>
             <form>
-                <input required type="text" placeholder='UserName'/>
-                <input required type="email" placeholder='Email'/>
-                <input required type="password" placeholder='Password'/>
-                <button>Register</button>
-                <p>Error de Registro!!</p>
+                <input required type="text" placeholder='UserName' name='username' onChange={handleChange}/>
+                <input required type="email" placeholder='Email' name="email" onChange={handleChange}/>
+                <input required type="password" placeholder='Password' name="password" onChange={handleChange}/>
+                <button onClick={handleSumit}>Register</button>
+                {err && <p>{err}</p>}
                 <span><Link to="/login">Login</Link></span>
             </form>
         </div>
