2a88b197e8b5b89a88fe69c9944418fe7f72b7c9
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-01-16 08:46:14 -0300

    Update Login.jsx

--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -1,16 +1,47 @@
-import React from 'react'
-import {Link} from "react-router-dom"
+import React, {useState} from 'react'
+import {Link, useNavigate} from "react-router-dom"
 import "./Login.css"
+import { useContext } from 'react';
+import { AuthContext } from '../../context/authContext';
 
 const Login = () => {
+    const [inputs, setInputs] = useState({
+        username: "",
+        password: "",
+    });
+
+    const [err, setErr] = useState(null);
+
+    const navigate = useNavigate();
+
+    const { login } = useContext(AuthContext); 
+
+    //atualizando os input state de acordo com oq o usuario escreve
+    const handleChange = (e) => {
+        setInputs(prev => ({...prev, [e.target.name]: e.target.value}))
+    }
+
+    //caso esteja tudo ok usamos o useNavigate para passar a gente para a tela de login
+    //caso tenha algum error de já existir o email, passamos para o err state e caso ele exista a gente renderiza na tela
+    const handleSumit =  async (e) => {
+        e.preventDefault();
+        try {
+            await login(inputs)
+            navigate("/");
+        } catch (error) {
+            setErr(error.response.data);
+        }
+    }
+
+
     return (
         <div className='auth'>
             <h1>Login</h1>
             <form>
-                <input type="text" placeholder='UserName'/>
-                <input type="password" placeholder='Password'/>
-                <button>Login</button>
-                <p>Error de Login!!</p>
+                <input type="text" placeholder='UserName' name='username' onChange={handleChange}/>
+                <input type="password" placeholder='Password' name='password' onChange={handleChange}/>
+                <button onClick={handleSumit}>Login</button>
+                {err && <p>{err}</p>}
                 <span><Link to="/register">Registre-se</Link></span>
             </form>
         </div>
