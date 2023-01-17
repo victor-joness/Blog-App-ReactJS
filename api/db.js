628316166cc008e70ee91ecd60b044cf55c08570
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-01-17 08:46:53 -0300

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,3 +1,3 @@
 import mysql from "mysql2"
 
-export const db = mysql.createConnection({host:"localhost", user:"root", password: process.env.REACT_APP_PASSWORD, database:"testes-blog-app"})
+export const db = mysql.createConnection({host:"localhost", user:"root", password: SENHADATABASE, database:"testes-blog-app"})
