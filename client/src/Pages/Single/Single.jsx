75fe8ba6602d10a226f668362dd8fdd1b91f2d88
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-01-18 10:11:22 -0300

    Update Single.jsx

--- client/src/Pages/Single/Single.jsx
+++ client/src/Pages/Single/Single.jsx
@@ -1,67 +1,62 @@
-import React, { useEffect, useId, useState } from "react";
-import { Link, Navigate, useLocation, useNavigate } from "react-router-dom";
-
-import Menu from "../../Components/Menu/Menu";
-
-import "./Single.css";
-
+import React, { useEffect, useState } from "react";
 import Edit from "../../img/edit.png";
 import Delete from "../../img/delete.png";
-import { useContext } from "react";
-import { AuthContext } from "../../context/authContext";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import Menu from "../../Components/Menu/Menu";
 import axios from "axios";
 import moment from "moment";
+import { useContext } from "react";
+import { AuthContext } from "../../context/authContext";
+import DOMPurify from "dompurify";
+import "./Single.css"
 
 const Single = () => {
-  //apenas 1 post que vai ser renderizado então tiramos o [] e colocamos o {}
   const [post, setPost] = useState({});
 
-  //useLocation pra pegar a url do post, esse url vem junto com o id router.get("/:id", getPost);
   const location = useLocation();
   const navigate = useNavigate();
 
-  //pegando o postid
   const postId = location.pathname.split("/")[2];
 
-  //user que está logado atualmente, usado para mostrar ou nao as opções de editar e delete
   const { currentUser } = useContext(AuthContext);
 
-  //useEffect que é para execulta cada vez que for renderizado, ele pega o postId pelo location.pathname e filtra todos os posts por isso
   useEffect(() => {
     const fetchData = async () => {
       try {
         const res = await axios.get(`/posts/${postId}`);
         setPost(res.data);
-      } catch (error) {
-        console.log(error);
+      } catch (err) {
+        console.log(err);
       }
     };
     fetchData();
   }, [postId]);
 
-  //funcao que faz a requisição de delete direto na endpoint, ou seja apagando o post
-  const handleDelete = async () => {
+  const handleDelete = async ()=>{
     try {
-        await axios.delete(`/posts/${postId}`);
-        navigate("/")        
-    } catch (error) {
-        console.log(error)
+      await axios.delete(`/posts/${postId}`);
+      navigate("/")
+    } catch (err) {
+      console.log(err);
     }
   }
 
   return (
     <div className="single">
       <div className="content">
-        <img src={post?.img} alt="" />
+        <img src={`../upload/${post?.img}`} alt="" />
         <div className="user">
-          {post.userImg && <img src={post.userImg} alt="" />}
-          <div className="infos">
-            <span>{post.username}</span>
+          {post.userImg && <img
+            src={post.userImg}
+            alt=""
+          />}
+          <div className="info">
+            <span>{post?.username}</span>
             <p>Posted {moment(post.date).fromNow()}</p>
           </div>
-          {currentUser.username === post.username && (
+          {currentUser?.username === post?.username && (
             <div className="edit">
-              <Link to={`/write?edit=2`}>
+              <Link to={`/write?edit=2`} state={post}>
                 <img src={Edit} alt="" />
               </Link>
               <img onClick={handleDelete} src={Delete} alt="" />
@@ -69,9 +64,12 @@ const Single = () => {
           )}
         </div>
         <h1>{post.title}</h1>
-        {post.desc}
-      </div>
-      <Menu />
+        <p
+          dangerouslySetInnerHTML={{
+            __html: DOMPurify.sanitize(post.desc),
+          }}
+        ></p>      </div>
+      <Menu cat={post.cat}/>
     </div>
   );
 };
