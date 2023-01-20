b1f3f3fc02843ee3f65fbaf1a2455a281eaec338
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-01-20 13:26:00 -0300

    Update Footer.jsx

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
+import Logo from "../../img/logo.png"
+import "./Footer.css"
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <img src={Logo} alt="" />
+      <span>Created by <span className='tagName'>Victor Mesquita</span></span>
+    </footer>
   )
 }
 
